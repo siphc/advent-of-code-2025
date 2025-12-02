f71cd4128eb95563563ad01d2e45c8eefc25844a
siphc <ayfpan@ucla.edu>
2025-12-01 21:46:59 -0800

    Day 1, A & B

--- Makefile
+++ Makefile
@@ -1,8 +1,11 @@
 .PHONY = clean
 CC = gcc
-LDLIBS = -lcurl
 
 get_input: get_input.o
+	$(CC) $(LDFLAGS) $^ -lcurl -o $@
+
+d1: d1.o parse_input.o
+	$(CC) $(LDFLAGS) $^ -lm -o $@
 
 clean:
-	rm -v *.o get_input
+	rm -v *.o get_input d?
---
+++ d1.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include "parse_input.h"
+
+int main() {
+	Matrix *m = matrix_parse("1.input");
+
+	int curr = 50;
+	int res_1 = 0;
+	int res_2 = 0;
+
+	for (int i=0; i<matrix_rows(m); i++) {
+		char *rotation = matrix_get(m, i, 0);
+		int clicks = atoi(rotation+1);
+
+		if (rotation[0] == 'L') {
+			res_2 += clicks>=curr%100 ? (clicks-curr%100)/100+1 : 0;
+			if (!(curr % 100)) res_2--;
+			curr -= clicks;
+		}
+		else {
+			res_2 += clicks+curr%100>=100 ? (clicks+curr%100)/100 : 0;
+			curr += clicks;
+		}
+
+		if (!(curr % 100))
+			res_1++;	
+	}
+
+	printf("Part 1: %d\n", res_1);
+	printf("Part 2: %d\n", res_2);
+
+	return 0;
+}
